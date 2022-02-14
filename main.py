7a1da716ded500e4d207cd75abfcf1197f39ceb6
Sergej V <vasiliokas@gmail.com>
2022-02-14 14:35:08 +0000

    simple script

---
+++ main.py
@@ -0,0 +1,9 @@
+def Add(a, b):
+        return a + b
+
+def SayHello():
+        print("Hello in 2022")
+        print("File __name__ = %s" %__name__)
+
+if __name__ == '__main__':
+        SayHello()
