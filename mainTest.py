d4b21276d06767a9a7944e5489c81e3242f6cb5e
Sergej V <vasiliokas@gmail.com>
2022-02-14 15:19:43 +0000

    fail assert test

--- mainTest.py
+++ mainTest.py
@@ -2,6 +2,7 @@ from main import Add
 
 def TestAdd():
   assert Add(3,6) == 9, "Assert Failed, number doesnt match"
+  assert Add(6,5) == 12
   print("Assert Add Pass")
 
 if __name__ == '__main__':
