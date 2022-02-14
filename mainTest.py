f2ce8f9a6bfb76161b611ef068f243a792a1006b
Sergej V <vasiliokas@gmail.com>
2022-02-14 14:40:25 +0000

    script Test

--- mainTest.py
+++ mainTest.py
@@ -1,9 +1,8 @@
-# Import the Add function, and assert that it works correctly.
 from main import Add
 
 def TestAdd():
-  assert Add(2,3) == 5
-  print("Add Function works correctly")
+  assert Add(3,6) == 9, "Assert Failed, number doesnt match"
+  print("Assert Add Pass")
 
 if __name__ == '__main__':
   TestAdd()
